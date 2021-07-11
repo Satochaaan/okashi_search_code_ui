b75e4b4c510bb447da6a76bdead760c1777abaac
Satochaaan <k97satosi327@gmail.com>
2021-07-11 17:48:26 +0900

    initial commit

---
+++ okashiCodeUI/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  okashiCodeUI
+//
+//  Created by 川野智史 on 2021/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // 背景色変更
+        self.view.backgroundColor = UIColor.green
+    }
+
+
+}
+
