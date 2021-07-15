95a4e34a47a9cd74dc3251e706cdc400210142e7
Satochaaan <k97satosi327@gmail.com>
2021-07-16 07:50:11 +0900

    コードでレイアウトの練習

---
+++ okashiCodeUI/SecondViewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+// ViewControllerを継承する
+class SecondViewController: ViewController {
+
+    // MARK: Views
+
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 背景色を変更
+        self.view.backgroundColor = UIColor.red
+        // ラベルの文字を変更
+        self.label.text = "みんなは何か書いた？"
+        // ボタンの文字を変更
+        self.button.setTitle("Back", for: .normal)
+    }
+
+    @objc override func buttonDidTap(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
--- okashiCodeUI/ViewController.swift
+++ okashiCodeUI/ViewController.swift
@@ -8,14 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let label = UILabel()
+    let button = UIButton()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // 背景色変更
         self.view.backgroundColor = UIColor.green
+        
+        // ラベル配置
+        self.label.text = "label置いてみた!!!"
+        self.label.translatesAutoresizingMaskIntoConstraints = false
+        // ラベルをviewに設置
+        self.view.addSubview(self.label)
+        // ラベルの位置を設定
+        self.label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        
+        // ボタン配置
+        self.view.addSubview(self.button)
+        self.button.translatesAutoresizingMaskIntoConstraints = false
+        self.button.setTitle("Next", for: .normal)
+        self.button.addTarget(self, action: #selector(self.buttonDidTap(_:)), for: .touchUpInside)
+        self.button.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        self.button.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -100).isActive = true
+        
+        
     }
 
-
+    // ボタンタップ処理
+    @objc func buttonDidTap(_ sender: UIButton) {
+        let secondViewController = SecondViewController()
+        self.present(secondViewController, animated: true, completion: nil)
+    }
 }
 
